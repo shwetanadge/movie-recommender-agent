d85c1927955ccbd3e1f515a8ac9953e5d943b1df
Shweta Nadge <shwetawsdev@gmail.com>
2026-06-08 10:37:45 +0530

    Added gitignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
+from pydantic import Field, BaseModel
 import os
 
 #load env file
@@ -10,3 +11,6 @@ llm = ChatGroq(
     model="llama-3.3-70b-versatile",
     api_key=os.getenv("YOUR_GROQ_API_KEY")
 )
+
+#Give structure to llm
+class MovieRecommender(BaseModel):
