a0b1d42ac9bfe2a48b8165e9f6a840a3d923f0cc
Shweta Nadge <shwetawsdev@gmail.com>
2026-06-10 08:13:15 +0530

    Defined structure to LLM

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ llm = ChatGroq(
 
 #Give structure to llm
 class MovieRecommender(BaseModel):
+    movie_names: list[str] = Field(description = "Movie Name")
+    genre: str = Field(description = "Movie Genre")
+    Industry: list[str] = Field(description="Specify which industry does the movie comes from?")
+    year_range: list[str] = Field(description="year range in which you want that movie from")
+
