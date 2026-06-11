944ce9646de0178fe2da089d7efa1a666dc9e2e1
Shweta Nadge <shwetawsdev@gmail.com>
2026-06-11 18:50:59 +0530

    Set sysystem prompt and message structure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from pydantic import Field, BaseModel
+from langchain_core.messages import SystemMessage, HumanMessage
 import os
 
 #load env file
@@ -18,4 +19,16 @@ class MovieRecommender(BaseModel):
     genre: str = Field(description = "Movie Genre")
     Industry: list[str] = Field(description="Specify which industry does the movie comes from?")
     year_range: list[str] = Field(description="year range in which you want that movie from")
+
+#System prompt
+system_prompt = """
+You are a movie recommender.
+You will search across the internet the list of movies with reference to the given input by the user.
+    """
+
+#messages
+messages = [
+    SystemMessage(content=system_prompt),
+    HumanMessage(content="Find me a movie from hollywood with thriller genre between 2000 to 2010")
+]
 
